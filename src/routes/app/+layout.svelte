6ae6cd313fa92952b997663189d91017f3648788
Tobias <tobias@sernhede.com>
2024-04-24 11:15:21 +0200

    x

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,31 +1,22 @@
 <script lang="ts">
+import { PUBLIC_LOGROCKET_KEY } from '$env/static/public';
+	import LogRocket from 'logrocket';
 	import { ModeWatcher } from 'mode-watcher';
 
 	import { Toaster } from '$lib/components/ui/sonner';
 	import { dev } from '$app/environment';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		if (!dev && PUBLIC_LOGROCKET_KEY) {
+			LogRocket.init(PUBLIC_LOGROCKET_KEY);
+		}
+	});
 </script>
 
 <ModeWatcher />
 <Toaster />
 
-<svelte:head>
-	{#if !dev}
-		<script type="text/javascript">
-			(function (c, l, a, r, i, t, y) {
-				c[a] =
-					c[a] ||
-					function () {
-						(c[a].q = c[a].q || []).push(arguments);
-					};
-				t = l.createElement(r);
-				t.async = 1;
-				t.src = 'https://www.clarity.ms/tag/' + i;
-				y = l.getElementsByTagName(r)[0];
-				y.parentNode.insertBefore(t, y);
-			})(window, document, 'clarity', 'script', 'm1ir2muhwe');
-		</script>
-	{/if}
-</svelte:head>
 <slot />
 
 <style lang="postcss">
