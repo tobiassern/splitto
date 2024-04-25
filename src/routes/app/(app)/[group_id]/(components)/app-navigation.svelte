024455b10ad391fc7a91542a6bc4027c1977ab15
Tobias <tobias@sernhede.com>
2024-04-25 13:53:04 +0200

    x

--- src/routes/app/(app)/[group_id]/(components)/app-navigation.svelte
+++ src/routes/app/(app)/[group_id]/(components)/app-navigation.svelte
@@ -17,7 +17,7 @@
 </script>
 
 <aside
-	class="fixed bottom-0 left-0 z-30 flex h-14 w-full justify-center border-r border-t bg-background sm:h-dvh sm:w-14 sm:border-t-0"
+	class="fixed bottom-0 left-0 z-30 flex h-[calc(env(safe-area-inset-bottom)_+_3.5rem)] w-full justify-center border-r border-t bg-background sm:h-dvh sm:w-14 sm:border-t-0"
 >
 	<nav
 		class="flex items-center justify-between gap-6 px-4 sm:flex-col sm:justify-start sm:gap-4 sm:px-0 sm:py-4"
--- src/routes/app/(app)/[group_id]/(components)/app-shell.svelte
+++ src/routes/app/(app)/[group_id]/(components)/app-shell.svelte
@@ -4,7 +4,9 @@
 	export let user: import('lucia').User | null;
 </script>
 
-<div class="flex min-h-dvh w-full flex-col pb-14 sm:pb-0 sm:pl-14">
+<div
+	class="pb-[calc(env(safe-area-inset-bottom)_+_3.5rem)] flex min-h-dvh w-full flex-col sm:pb-0 sm:pl-14"
+>
 	<main class="mx-auto grid w-full max-w-7xl grid-cols-12 items-start gap-4 p-4 sm:p-6 md:gap-8">
 		<slot />
 	</main>
