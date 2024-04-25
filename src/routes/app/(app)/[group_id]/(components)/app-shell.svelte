a27da359af88f8e9b7b18858458510431fe244a8
Tobias <tobias@sernhede.com>
2024-04-25 14:07:50 +0200

    x

--- src/routes/app/(app)/[group_id]/(components)/app-shell.svelte
+++ src/routes/app/(app)/[group_id]/(components)/app-shell.svelte
@@ -8,7 +8,9 @@
 	class="flex min-h-dvh w-full flex-col sm:pb-0 sm:pl-14"
 	style="padding-bottom: calc(env(safe-area-inset-bottom) + 3.5rem);"
 >
-	<main class="mx-auto grid w-full max-w-7xl grid-cols-12 items-start gap-4 p-4 sm:p-6 md:gap-8">
+	<main
+		class="mx-auto grid w-full max-w-7xl grid-cols-12 items-start gap-4 p-4 pt-[calc(env(safe-area-inset-top)_+_1rem)] sm:p-6 md:gap-8"
+	>
 		<slot />
 	</main>
 </div>
